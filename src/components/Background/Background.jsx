c34405e7b8c286c9c07ee7272fcf4ba3d7d898a7
Shaqs0 <hhgfhhhggb@gmail.com>
2024-02-08 22:52:40 +0300

    /

--- src/components/Background/Background.jsx
+++ src/components/Background/Background.jsx
@@ -1,8 +1,20 @@
 import styles from './Background.module.css';
 
 function Background() {
-	return(
-		<img className={styles.picture} src='/Rectangle 23.png'></img>
-	);}
+	return (
+		<>
+			<div className={styles.picture_container}>
+				<img className='' src='/Rectangle 23.png' alt="Background"></img>
+
+			</div>
+			<div className={styles.texth1}>Тавимский морской порт VISMA</div>
+			<div className={styles.texth3}>Vel posuere nibh odio placerat massa vel tellus tortor. Varius eget nunc scelerisque etiam felis facilisi ante viverra sem. Nunc eros elementum.</div>
+			<div className={styles.other}></div>
+			<div className={styles.buttons}>
+				<button className={styles.big_button}></button>
+			</div>
+		</>
+	);
+}
 
 export default Background;
